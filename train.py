af05bbba7373d4fa83d17e32023b6746dd42c704
Li Tang <38803555+tanglijhu@users.noreply.github.com>
2020-12-17 03:53:28 +0000

    Add files via upload

---
+++ train.py
@@ -0,0 +1,71 @@
+# ## Configure Azure resources
+
+from sklearn.linear_model import LogisticRegression
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+# ## Retrive current run's information
+
+# +
+run = Run.get_context()
+
+ws = run.experiment.workspace
+# +
+found = False
+key = "heart-failure"
+description_text = "Clinical Record DataSet for Predicting Death Event"
+
+if key in ws.datasets.keys(): 
+        found = True
+        dataset = ws.datasets[key] 
+#dataset
+# -
+
+
+# ## Split data into train and test set
+
+def clean_data(data):
+
+    # Clean and one hot encode data
+    x_df = data.to_pandas_dataframe().dropna()
+
+
+    y_df = x_df.pop("DEATH_EVENT")
+    return x_df , y_df
+
+x, y = clean_data(dataset)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=42)
+
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
+    
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model.joblib')
+
+
+if __name__ == '__main__':
+    main()
